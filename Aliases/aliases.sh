5c875463cfb0eabb714bf4ae546c051e643db20e
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-04 17:16:31 -0300

    Update aliases.sh

--- Aliases/aliases.sh
+++ Aliases/aliases.sh
@@ -2,7 +2,13 @@ clear
 cd ~/
 echo "Creating alias..."
 :> ~/.bashrc
-echo 'alias c="clear"' > ~/.bashrc
-echo 'alias sd="cd /sdcard/"' > ~/.bashrc
-source ~/.bashrc
+echo 'alias c="clear"' >> ~/.bashrc
+echo 'alias sd="cd /sdcard/"' >> ~/.bashrc
+echo 'alias py="python"' >> ~/.bashrc
+echo 'alias njs="node"' >> ~/.bashrc
+
+clear
+# If not work, restart your termux:
+cd ~/
+source .bashrc
 clear
