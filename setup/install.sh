f25d70d6223cd9c4b92b2ca55dafad8f9832d7f2
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-04 16:41:56 -0300

    Create install.sh

---
+++ setup/install.sh
@@ -0,0 +1,20 @@
+cd ~/
+clear
+echo "Installing Python & PIP"
+pkg install python python-pip -y
+clear
+echo "Installing NodeJS & NPM"
+pkg isntall nodejs -y
+pkg install nodejs
+clear
+echo "Installing Micro Editor"
+pkg install micro -y
+clear
+echo "Installing Vim & Neovim"
+pkg install vim neovim -y
+clear
+echo "Installing Openssh & Proot-distro"
+pkg install proot-distro openssh -y
+clear
+
+echo "Everything installed. Now go to the 'aliases' folder to place the aliases/shortcuts."
