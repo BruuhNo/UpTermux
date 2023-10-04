b491760def458fbf53b6f2712f17b8c2b921a02b
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-04 16:08:02 -0300

    Create setup.sh

---
+++ setup/setup.sh
@@ -0,0 +1,10 @@
+clear && echo "Updating packages..."
+echo "To cancel press 'CTRL + C'"
+sleep 1.5
+pkg update -y && clear
+clear
+termux-setup-storage
+echo "Press 'Allow'"
+pkg upgrade -y && clear
+clear
+echo "Everything updated, now run 'install.sh'"
