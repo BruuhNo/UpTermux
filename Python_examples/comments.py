8838f71a459e7054e106345c2bb9d4f5a070e1d3
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-04 21:15:32 -0300

    Create comments.py
    A comments example in Python

---
+++ Python_examples/comments.py
@@ -0,0 +1,10 @@
+# This is a comment.
+
+# A comment can be defined using a "#"
+# This is an example of a comment, with a "#" in front of the word/phrase
+
+# Believe me, I know these things (the "#" comments) may seem stupid and pointless, but they really, really help you understand your code.
+
+# Ignore the code below
+print("Go edit this file to understand about comments (trust me, they are extremely important).")
+exit()
