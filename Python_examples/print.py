134305c5482d3f3f468a85dfe63ebaa8757abbad
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-04 21:25:24 -0300

    Create print.py

---
+++ Python_examples/print.py
@@ -0,0 +1,17 @@
+# The "print()"
+# See an example below:
+print("This is an example of print()")
+
+# The output of the above code would be => "This is an example of print()"
+# Why?  Because "print()" is used to PRINT / show what is between the parentheses.
+
+# See another example:
+print("Hello World!")
+# Output => Hello World
+
+# Another example:
+print(1+1)
+# Output => 2
+
+# Ignore the code below
+print("Edit or view this file in an editor to understand...")
