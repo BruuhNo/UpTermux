1c014b6745d3f69cf092eb68477070796c7ddb2f
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-05 11:26:12 -0300

    Update install.sh
    More packages... Total: 19 Packages

--- setup/install.sh
+++ setup/install.sh
@@ -15,6 +15,21 @@ clear
 echo "Installing Openssh & Proot-distro"
 pkg install proot-distro openssh -y
 clear
+echo "Installing Curl, Wget & Neofetch"
+pkg install curl wget neofetch -y
+clear
+echo "Installing Zip & Unzip"
+pkg install zip unzip -y
+clear
+echo "Installing Php & Ruby"
+pkg install php ruby -y
+clear
+echo "Installing Sqlite & Apache2"
+pkg install sqlite apache2 -y
+clear
+echo "Installing Cmatrix & FFMPEG"
+pkg install cmatrix ffmpeg -y
+clear
 
 echo "Everything installed. Now go to the 'aliases' folder to place the aliases/shortcuts: cd ~/UpTermux/Aliases."
 echo "After this, execute: 'chmod +x *'."
