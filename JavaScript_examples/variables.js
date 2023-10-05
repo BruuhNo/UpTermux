bd2d3462d9cb6fa6917447bb051ca9d5e7ff86de
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-05 18:48:48 -0300

    Create variables.js

---
+++ JavaScript_examples/variables.js
@@ -0,0 +1,10 @@
+// Creating variable with 'var'
+var year = 2023;
+
+// Creating variable with 'let'
+let month = 10;
+
+// Creating variable with 'const'(Unchangeable value)
+const day = 05;
+
+console.log(month, day, year);
