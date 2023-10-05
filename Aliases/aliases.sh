1469b5e0b0659c458666b290d934c5a51b68db22
imbrunao <123264874+imbrunao@users.noreply.github.com>
2023-10-05 18:36:50 -0300

    Update aliases.sh
    Updating aliases.sh

--- Aliases/aliases.sh
+++ Aliases/aliases.sh
@@ -1,14 +1,28 @@
+# Clear the screen for a clean start
 clear
-cd ~/
-echo "Creating alias..."
-:> ~/.bashrc
-echo 'alias c="clear"' >> ~/.bashrc
-echo 'alias sd="cd /sdcard/"' >> ~/.bashrc
-echo 'alias py="python"' >> ~/.bashrc
-echo 'alias njs="node"' >> ~/.bashrc
 
-clear
-# If not work, restart your termux:
+# Navigate to the home directory
 cd ~/
-source .bashrc
+
+# Inform the user that we're creating aliases
+echo "Creating aliases..."
+
+# Create or overwrite the .bashrc file to add aliases
+:> ~/.bashrc
+
+# Add aliases to .bashrc with clear explanations
+echo 'alias c="clear"' >> ~/.bashrc
+echo 'alias sd="cd /sdcard/" # Shortcut to navigate to the /sdcard/ directory' >> ~/.bashrc
+echo 'alias py="python"     # Shortcut for Python interpreter' >> ~/.bashrc
+echo 'alias njs="node"     # Shortcut for Node.js' >> ~/.bashrc
+
+# Clear the screen again
 clear
+
+# Source the updated .bashrc to activate the aliases
+source ~/.bashrc
+
+# Inform the user to restart Termux if necessary
+echo "Your aliases are ready to use."
+echo " "
+echo "If the aliases don't work, please restart your Termux."
